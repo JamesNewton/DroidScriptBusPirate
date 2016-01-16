a2348f4f6c3d8919a6f7a6a9ee96643dd5a1efbb
JamesNewton <jamesnewton@geocities.com>
2016-01-15 19:50:23 -0800

    Add Scale and Calibration to Voltmeter, Mode tracking.
    Big update! Adds a scaling factor to the value displayed in Voltmeter mode, and a calibrate button which powers up the internal supplies and explains how to connect a potentiometer so that the input voltage can be scaled, and the +5 power supply can be used as a voltage reference to calibrate the potentiometer. In order to automatically turn on the power supply, the app now tracks the BusPirate mode, and if it's in HiZ, it automatically switches to 1-Wire (can't turn on power in HiZ) and then switches back when finished.

--- BusPirate.js
+++ BusPirate.js
@@ -1,16 +1,19 @@
 // Buspirate app. 
-// 
+// http://techref.massmind.org/techref/language/DroidScript/BusPirate/index.htm
 // Note: this application only works on devices that support  
 // OTG and allow access to external serial devices. 
 // 
-// Known to work: Nexus7, GalaxyS3/S4, ExperiaZUltra, TescoHudl    
-// Don't work: Nexus4, GalaxyS1
+// Known to work: Nexus7, GalaxyS3/S4, ExperiaZUltra, TescoHudl,  Asus MemoPad 8  
+// Don't work: Nexus4, GalaxyS1, AsusMemo
 
 //Global variables. 
 var usb=null, reply=""; 
 var log="", maxLines;
 var txts="", descs=[], cmds=[], ary;
 var desc="";
+var modes=" HiZ>1-WIRE>UART>I2C>SPI>2WIRE>3WIRE>KEYB>LCD>";
+var mode="";
+var scaleADC=1;
 var aryParm=[];
 
 //Called when application is started. 
@@ -23,7 +26,7 @@ function OnStart() {
         txts +="Reset\t#\r";
         txts +="Show Pin State\tv\r";
         txts +="ADC In\td\r";
-        txts +="ADC Loop\tD\r";
+        txts +="ADC Loop (Voltimeter)\tD\r";
         txts +="Delay 1uS\t&\r";
         txts +="Delay 1mS\t%\r";
         txts +="Frequency Counter\tf\r";
@@ -63,8 +66,7 @@ function OnStart() {
         txts +=" #CS 1=high 2=low";
         txts +=" #Output 1=open_collecter 2=normal";
         txts +=" \tm7\r";
-        txts +="Keyboard Mode\tm8\r";
-        txts +="LCD Mode\tm9\r";
+        txts +="LCD Mode\tm8\r";
         txts +="Bitorder MSB\tl\r";
         txts +="Bitorder LSB\tL\r";
         txts +="HEX Display\to1\r";
@@ -329,44 +331,6 @@ function findNextParm(item, start) {
     }
 
 
-//called when user selects ADC loop mode (D)
-function btnMultimeter_OnTouch() { 
-    dlgTxt = app.CreateDialog( "Multimeter mode" );
-        
-    //Create a layout for dialog.
-    layDlg = app.CreateLayout( "linear", "Vertical,Center" );
-    dlgTxt.AddLayout( layDlg );
-
-    //layDlg.SetPadding( 0.02, 0, 0.02, 0.02 );
-    //dlgTxt.AddLayout( layDlg );
-    //layDlgL1 = app.CreateLayout( "linear", "Horizontal,Left" );
-    //layDlg.AddChild(layDlgL1);
-
-    //Create a read-only edit box to show responses. 
-    edtVolts = app.CreateText( "0.00", 0.95, 0.15, "Right, Monospace" ); 
-    edtVolts.SetMargins( 0,0,0,0.01 ); 
-    edtVolts.SetBackColor( "#333333" );
-    edtVolts.SetTextSize( 56 ); 
-    layDlg.AddChild( edtVolts );
-
-    //Create an ok button. 
-    btnMultimeterOk  = app.CreateButton( "Exit", 0.23, 0.1 ); 
-    btnMultimeterOk.SetOnTouch( btnMultimeterOk_OnTouch ); 
-    layDlg.AddChild( btnMultimeterOk ); 
-     
-    //Show dialog.
-    dlgTxt.Show();
-    Send("d");
-    }
-
-function btnMultimeterOk_OnTouch() { 
-    layDlg.DestroyChild(edtVolts);
-    edtVolts = undefined;
-    dlgTxt.Hide();
-    Send("d");
-    }
-
-
 //called when the user selects UART mode
 function btnUart_OnTouch() {  
     var txt="";
@@ -429,6 +393,90 @@ function btnUartOk_OnTouch(item) {
     dlgTxt.Hide();
     } 
 
+//called when user selects ADC loop mode (D)
+function btnMultimeter_OnTouch() { 
+    dlgTxt = app.CreateDialog( "Multimeter mode" );
+        
+    //Create a layout for dialog.
+    layDlg = app.CreateLayout( "linear", "Vertical,Center" );
+    dlgTxt.AddLayout( layDlg );
+
+    //Create a read-only edit box to show responses. 
+    edtVolts = app.CreateText( "0.00", 0.95, 0.15, "Right, Monospace" ); 
+    edtVolts.SetMargins( 0,0,0,0.01 ); 
+    edtVolts.SetBackColor( "#333333" );
+    edtVolts.SetTextSize( 56 ); 
+    layDlg.AddChild( edtVolts );
+  
+    layDlgHoriz = app.CreateLayout( "Linear", "Horizontal" );
+    layDlg.AddChild( layDlgHoriz );
+    layDlgHoriz.AddChild( app.CreateText( "Scale:" ) );
+    
+    spinVolts = app.CreateSpinner( "x1,x5,x10,x25,x50,x100", 0.2 );
+    spinVolts.SetOnChange( spinVolts_onTouch );
+    layDlgHoriz.AddChild( spinVolts );
+
+    btnScaleCal  = app.CreateButton( "Calibrate", 0.3, 0.1 ); 
+    btnScaleCal.SetOnTouch( btnScaleCal_OnTouch ); 
+    layDlgHoriz.AddChild( btnScaleCal ); 
+    layDlg.AddChild( app.CreateText( "(Calibrate will turn on power supplies!)" ) );
+    txtScaleCal = null;
+
+    //Create an ok button. 
+    btnMultimeterOk  = app.CreateButton( "Exit", 0.23, 0.1 ); 
+    btnMultimeterOk.SetOnTouch( btnMultimeterOk_OnTouch ); 
+    layDlg.AddChild( btnMultimeterOk ); 
+     
+    //Show dialog.
+    dlgTxt.Show();
+    Send("d");
+    }
+
+function btnScaleCal_OnTouch() {
+    if (!txtScaleCal) { //if we haven't already entered this mode
+        layDlg.mode=mode; //save the starting mode locally
+        if ("HiZ>"==mode) { 
+            Send("m2");
+            app.ShowPopup("No power in HiZ, switching to 1-WIRE");
+            }
+        Send("W"); //Power UP!
+        txtScaleCal = app.CreateText( //teach user to connect scale pot
+             "1. Connect +5 volt power and ground across potentiometer\n"
+            +"2. Connect ADC to potentiometer wiper\n"
+            +"3. Select desired scale\n"
+            +"4. Trim potentiometer to show power supply voltage (5 V)\n"
+            +"5. Disconnect +5 volt power from potentiometer\n"
+            +"6. Connect voltage to be measured\n"
+            +"(click Calibrate to close)\n"
+            ,0.8, 0.4, "Left, Multiline");
+        layDlg.AddChild( txtScaleCal );
+        }
+    else { //if we are already in calibrate mode, get out
+        if ("HiZ>"==layDlg.mode) { 
+            Send("m1");
+            app.ShowPopup("Switching back to HiZ");
+            }
+        Send("w"); //Power Down
+        layDlg.DestroyChild(txtScaleCal);
+        txtScaleCal = null;
+        }
+    }
+
+function spinVolts_onTouch( item ) {
+    scaleADC = item.substring(1); //scale is after the "x"
+    }
+    
+function btnMultimeterOk_OnTouch() { 
+    if (txtScaleCal) { //if we are still in calibration mode
+        btnScaleCal_OnTouch(); //pretend the user closed it
+        }
+    layDlg.DestroyChild(edtVolts); //destroy the edit box
+    edtVolts = undefined; //and forget it so we don't funnel responses there.
+    dlgTxt.Hide(); //hide the dialog
+    Send("d"); //one last reading so the user as a record of it.
+    }
+
+
 //Check connection and send data.  
 function Send( s ) { 
     if( usb ) usb.Write( s+"\r" ); 
@@ -437,19 +485,20 @@ function Send( s ) {
 
 //Called when we get data from the BusPirate
 function usb_OnReceive( data ) {
-    var i;
     //if we are in meter mode, display the data
     if (typeof(edtVolts)!='undefined') {
-        data += ":\n"; //ensure we will have delimeters
-        data = data.split(":")[1] .split("\n")[0];
-        edtVolts.SetText(data);
+        data += ":V"; //ensure we will have delimeters
+        data = parseFloat(data.split(":")[1] .split("V")[0]);
+        data = scaleADC*data;
+        edtVolts.SetText(data.toPrecision(3)+" V");
         Send("d"); //and repeat
         return;
         }
     //Need to translate tabs into spaces.
     var lines = data.split("\n");
+    var line; //line outside for scope so it is retained after loop ends
     for (var l = 0; l < lines.length; l++) {
-        var line="";
+        line="";
         tabs = lines[l].split("\t");
         for (var i = 0; i < tabs.length-1; i++) {
             line += tabs[i]+("        ").slice(0,8-(tabs[i].length % 8));
@@ -458,14 +507,19 @@ function usb_OnReceive( data ) {
         if ( l < lines.length - 1 ) line += "\n";
         log += line;
         }
+    //last line of returned data could be our mode.
+    if (modes.indexOf(line)>0 && mode!=line) {
+        mode=line; 
+        txt.SetText("BusPirate "+mode);
+        }
     //scroll extra lines off the top
     var logLines = log.split("\n");
-    //could we calculate the longest line and set font size accordingly?
+    //calculate the longest line and set font size accordingly
     var maxChars = 0;
     for (var line in logLines) {
         maxChars = Math.max(maxChars, logLines[line].length);
         }
-    //we want 8 point when the line is 80(?ish) and up to 14 point at 20.
+    //we want 8 point when the line is 80(?ish) and up to 14 point at 10.
     edtReply.SetTextSize(Math.min(16-maxChars/10,14));
     maxLines = edtReply.GetMaxLines()-1;
     logLines = logLines.slice( -maxLines );
